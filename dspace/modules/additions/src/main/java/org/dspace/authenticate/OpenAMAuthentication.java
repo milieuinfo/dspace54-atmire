4a0fdfaa012d9ceb39e027c01480023dbd3c6418
Bjorn Jaspers <bjornjaspers@gmail.com>
2016-04-14 15:59:08 +0200

    Added OpenAM related files, but this doesn't work yet as I didn't have access to the required dependencies

---
+++ dspace/modules/additions/src/main/java/org/dspace/authenticate/OpenAMAuthentication.java
@@ -0,0 +1,186 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.authenticate;
+
+import be.milieuinfo.security.openam.api.OpenAMUserdetails;
+import be.milieuinfo.security.openam.oauth.JerseyBasedOAuthIdentityService;
+import be.milieuinfo.security.openam.oauth.OAuthTokenPair;
+import com.sun.jersey.api.client.ClientRequest;
+import com.sun.jersey.api.client.ClientResponse;
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.core.ConfigurationManager;
+import org.dspace.core.Context;
+import org.dspace.core.LogManager;
+import org.dspace.eperson.EPerson;
+import org.dspace.eperson.Group;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public abstract class OpenAMAuthentication implements AuthenticationMethod {
+
+    private static Logger log = Logger.getLogger(OpenAMAuthentication.class);
+
+    private static final String ADMINISTRATOR_GROUP = "Administrator";
+    private static final String DSPACE_ADMIN_ROLE = "DSpaceAdmin";
+    private static final String DSPACE_ROLE_PREFIX = "DSpace";
+
+    protected DSpaceJerseyBasedOAuthIdentityService openAMIdentityService;
+
+    protected OpenAMAuthentication() {
+        final String openamServerUrl = ConfigurationManager.getProperty("authentication-openam", "openam.server.url");
+        final String consumerToken = ConfigurationManager.getProperty("authentication-openam", "openam.consumer.token");
+        final String consumerSecret = ConfigurationManager.getProperty("authentication-openam", "openam.consumer.secret");
+
+        this.openAMIdentityService = new DSpaceJerseyBasedOAuthIdentityService();
+        this.openAMIdentityService.setUrl(openamServerUrl);
+        this.openAMIdentityService.setConsumerToken(new OAuthTokenPair(consumerToken, consumerSecret));
+    }
+
+    public int authenticateOpenAM(Context context, HttpServletRequest request, String ssoId) throws SQLException {
+        if (!StringUtils.isBlank(ssoId)) {
+            final OpenAMUserdetails userDetails = this.openAMIdentityService.getUserDetails(ssoId);
+            if (userDetails != null) {
+                final String userName = userDetails.getUsername();
+            	
+            	final String email = userDetails.getAttributeValue("mail") == null ? userName : userDetails.getAttributeValue("mail");
+                final String sn = userDetails.getAttributeValue("sn") == null ? userName : userDetails.getAttributeValue("sn");
+                final String givenName = userDetails.getAttributeValue("givenName") ==null ? userName : userDetails.getAttributeValue("givenName");
+                
+                final Collection<String> roles = userDetails.getRoles();
+                if (!StringUtils.isBlank(email)) {
+                    try {
+                        final EPerson knownEPerson = EPerson.findByEmail(context, email);
+                        if (knownEPerson == null) {
+                            // TEMPORARILY turn off authorisation
+                            context.turnOffAuthorisationSystem();
+                            final EPerson eperson = createEPerson(context, request, email, sn, givenName);
+                            eperson.update();
+                            fixGroups(context, roles, eperson);
+                            context.commit();
+                            context.restoreAuthSystemState();
+                            context.setCurrentUser(eperson);
+                            log.info(LogManager.getHeader(context, "login", "type=openam-interactive"));
+                            return SUCCESS;
+                        } else {
+                        	fixGroups(context, roles, knownEPerson);
+                            context.setCurrentUser(knownEPerson);
+                            return SUCCESS;
+                        }
+                    } catch (AuthorizeException e) {
+                        log.warn(LogManager.getHeader(context, "authorize_exception", ""), e);
+                        return BAD_ARGS;
+                    }
+                } else {
+                    return BAD_ARGS;
+                }
+            } else {
+                return NO_SUCH_USER;
+            }
+        } else {
+            return NO_SUCH_USER;
+        }
+    }
+
+    protected EPerson createEPerson(Context context, HttpServletRequest request, String email, String sn, String givenName) throws SQLException, AuthorizeException {
+        final EPerson eperson = EPerson.create(context);
+        eperson.setFirstName(sn);
+        eperson.setLastName(givenName);
+        eperson.setEmail(email);
+        eperson.setRequireCertificate(false);
+        eperson.setSelfRegistered(true);
+        eperson.setCanLogIn(true);
+        AuthenticationManager.initEPerson(context, request, eperson);
+        return eperson;
+    }
+
+    
+
+    protected void fixGroups(Context context, Collection<String> roles , EPerson ePerson) throws SQLException, AuthorizeException {
+    	
+    	ArrayList<Group> currentGroups = new ArrayList<Group>();
+    	
+    	for (String role : roles) {
+            if(DSPACE_ADMIN_ROLE.equals(role)) {
+                final Group admins = Group.findByName(context, ADMINISTRATOR_GROUP);
+                if (admins != null) {
+                    admins.addMember(ePerson);
+                    admins.update();
+                    
+                    currentGroups.add(admins);
+                    
+                } else {
+                    log.warn(LogManager.getHeader(context, "login", "Could not add user as administrator (group not found)!"));
+                }
+            } else if(role.startsWith(DSPACE_ROLE_PREFIX)) {
+                final String groupName = role.replaceAll(DSPACE_ROLE_PREFIX, "");
+                final Group group = Group.findByName(context, groupName);
+                if (group != null) {
+                    group.addMember(ePerson);
+                    group.update();
+                    
+                    currentGroups.add(group);
+                    
+                } else {
+                    log.warn(LogManager.getHeader(context, "login", "Could not add user to group:" + groupName + " (group not found)!"));
+                }
+            }
+        }
+    	
+    	
+    	Group[] dbGroups = Group.allMemberGroups(context, ePerson);
+    	for (Group dbGroup : dbGroups ){
+    		if (dbGroup.getID() == 0 ){
+    			log.debug("Everybody belongs to the anonymous group");
+    		}else if (!currentGroups.contains(dbGroup)){
+    			log.info(ePerson.getName() + " belongs to group: "+ dbGroup.getName() + " in the database but not in LDAP, removing person from group");
+    			dbGroup.removeMember(ePerson);
+    			dbGroup.update();
+    		}
+    	}
+       	
+    }
+    
+    protected class DSpaceJerseyBasedOAuthIdentityService extends JerseyBasedOAuthIdentityService {
+
+        private static final String LOGIN_URL = BASE_PATH + "/login";
+
+        protected URI loginUrl;
+
+        public String login(String username, String password) {
+            ClientRequest clientRequest = ClientRequest.create().build(
+                    UriBuilder.fromUri(this.loginUrl).queryParam(USERNAME_QUERY_PARAM, username)
+                            .queryParam(PASSWORD_QUERY_PARAM, password).build(), DEFAULT_METHOD);
+            String token = null;
+            ClientResponse clientResponse = getClientResponse(clientRequest, getOAuthParameters(""), getOAuth2LeggedSecrets());
+            if (clientResponse.getStatus() == HttpServletResponse.SC_OK) {
+                token = getTokenId(clientResponse);
+            }
+            return token;
+        }
+
+
+        public void setUrl(String serverUrl) {
+            super.setServerUrl(serverUrl);
+            try {
+                this.loginUrl = new URI(serverUrl + LOGIN_URL);
+            } catch (URISyntaxException e) {
+                throw new IllegalArgumentException(e);
+            }
+        }
+    }
+
+}
---
+++ dspace/modules/additions/src/main/java/org/dspace/authenticate/OpenAMByPassAdmin.java
@@ -0,0 +1,65 @@
+package org.dspace.authenticate;
+
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.core.Context;
+import org.dspace.eperson.EPerson;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Bypass openam authentication (put it as first in the authentication chain and
+ * you will login automatically as admin)
+ * 
+ * @author lds
+ *
+ */
+public class OpenAMByPassAdmin extends OpenAMImplicitAuthentication {
+
+	@Override
+	public int authenticate(Context context, String username, String password,
+							String realm, HttpServletRequest request) throws SQLException {
+
+		final String email = "admin@milieuinfo.be";
+		final String sn = "Admin";
+		final String givenName ="Admin";
+		final Collection<String> roles = new ArrayList<String>();
+		roles.add("DSpaceAdmin");
+	
+			try {
+				final EPerson knownEPerson = EPerson
+						.findByEmail(context, email);
+				if (knownEPerson == null) {
+					// TEMPORARILY turn off authorisation
+					
+					context.turnOffAuthorisationSystem();
+					final EPerson eperson = createEPerson(context, request,email, sn, givenName);
+					eperson.update();
+					fixGroups(context, roles, eperson);
+					context.commit();
+					
+					context.restoreAuthSystemState();
+					context.setCurrentUser(eperson);
+
+					return SUCCESS;
+				} else {
+					fixGroups(context, roles, knownEPerson);
+					context.setCurrentUser(knownEPerson);
+					return SUCCESS;
+				}
+			} catch (AuthorizeException e) {
+			
+				return BAD_ARGS;
+			}
+		
+
+	}
+
+	@Override
+	protected String retrieveOpenAMToken(HttpServletRequest request) {
+		return null;
+	}
+
+}
