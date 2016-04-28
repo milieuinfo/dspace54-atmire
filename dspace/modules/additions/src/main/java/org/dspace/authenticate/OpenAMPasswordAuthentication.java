16240ff5f9c7ca09606ccec7ac2b0c58d2de60b4
Bjorn Jaspers <bjornjaspers@gmail.com>
2016-04-28 12:39:57 +0200

    Added missing OpenAM files and some missing modifications

---
+++ dspace/modules/additions/src/main/java/org/dspace/authenticate/OpenAMPasswordAuthentication.java
@@ -0,0 +1,123 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.authenticate;
+
+import org.apache.log4j.Logger;
+import org.dspace.core.Context;
+import org.dspace.eperson.EPerson;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+
+public class OpenAMPasswordAuthentication extends OpenAMAuthentication {
+
+    /** log4j category */
+    private static Logger log = Logger.getLogger(OpenAMPasswordAuthentication.class);
+
+    /**
+     * Registrations are nog handled by OpenAM.
+     *
+     * @param context
+     *            DSpace context
+     * @param request
+     *            HTTP request, in case it's needed. May be null.
+     * @param username
+     *            Username, if available.  May be null.
+     * @return in any case false
+     * @throws SQLException
+     */
+    @Override
+    public boolean canSelfRegister(Context context, HttpServletRequest request, String username) throws SQLException {
+        return true;
+    }
+
+    /**
+     * We are not changing user data...
+     *
+     * @param context
+     *            DSpace context
+     * @param request
+     *            HTTP request, in case it's needed. May be null.
+     * @param eperson
+     *            newly created EPerson record - email + information from the
+     * @throws SQLException
+     */
+    @Override
+    public void initEPerson(Context context, HttpServletRequest request, EPerson eperson) throws SQLException {
+        // Nothing to do here...
+    }
+
+    /**
+     * We are not changing user data...
+     *
+     * @param context
+     *            DSpace context
+     * @param request
+     *            HTTP request, in case it's needed. May be null.
+     * @param username
+     *            Username, if available.  May be null.
+     * @return in any case false
+     * @throws SQLException
+     */
+    @Override
+    public boolean allowSetPassword(Context context, HttpServletRequest request, String username) throws SQLException {
+        return false;
+    }
+
+    /**
+     * This is an explicit method, since it needs username and password
+     * from some source.
+     *
+     * @return in any case false
+     */
+    @Override
+    public boolean isImplicit() {
+        return false;
+    }
+
+    /**
+     * We are not changing user data...
+     *
+     * @param context
+     *            A valid DSpace context.
+     *
+     * @param request
+     *            The request that started this operation, or null if not
+     *            applicable.
+     *
+     * @return an empty array
+     *
+     * @throws SQLException
+     */
+    @Override
+    public int[] getSpecialGroups(Context context, HttpServletRequest request) throws SQLException {
+        return new int[0];
+    }
+
+    @Override
+    public int authenticate(Context context,
+                            String username,
+                            String password,
+                            String realm,
+                            HttpServletRequest request) throws SQLException {
+        final String ssoId = openAMIdentityService.login(username, password);
+        return authenticateOpenAM(context, request, ssoId);
+    }
+
+    @Override
+    public String loginPageURL(Context context, HttpServletRequest request, HttpServletResponse response) {
+        return response.encodeRedirectURL(request.getContextPath() + "/openam-login");
+    }
+
+    @Override
+    public String loginPageTitle(Context context) {
+        return "org.dspace.eperson.OpenAMPasswordAuthentication.title";
+    }
+
+}
---
+++ dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/eperson/OpenAMAuthenticateAction.java
@@ -0,0 +1,69 @@
+package org.dspace.app.xmlui.aspect.eperson;
+
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.cocoon.acting.AbstractAction;
+import org.apache.cocoon.environment.ObjectModelHelper;
+import org.apache.cocoon.environment.Redirector;
+import org.apache.cocoon.environment.Request;
+import org.apache.cocoon.environment.SourceResolver;
+import org.apache.cocoon.environment.http.HttpEnvironment;
+import org.apache.cocoon.sitemap.PatternException;
+import org.dspace.app.xmlui.utils.AuthenticationUtil;
+import org.dspace.core.Context;
+import org.dspace.eperson.EPerson;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+public class OpenAMAuthenticateAction extends AbstractAction {
+
+	/**
+	 * Attempt to authenticate the user.
+	 */
+	public Map act(Redirector redirector, SourceResolver resolver,	Map objectModel, String source, Parameters parameters)
+			throws Exception {
+		// First check if we are preforming a new login
+		Request request = ObjectModelHelper.getRequest(objectModel);
+
+		try {
+			Context context = AuthenticationUtil.authenticateImplicit(objectModel);
+
+			EPerson eperson = context.getCurrentUser();
+
+			if (eperson != null) {
+				// The user has successfully logged in
+				String redirectURL = request.getContextPath();
+
+				if (AuthenticationUtil.isInterupptedRequest(objectModel)) {
+					// Resume the request and set the redirect target URL to
+					// that of the originaly interrupted request.
+					redirectURL += AuthenticationUtil.resumeInterruptedRequest(objectModel);
+				} 
+
+				// Authentication successfull send a redirect.
+				final HttpServletResponse httpResponse = (HttpServletResponse) objectModel
+						.get(HttpEnvironment.HTTP_RESPONSE_OBJECT);
+
+				httpResponse.sendRedirect(redirectURL);
+
+				// log the user out for the rest of this current request,
+				// however they will be reauthenticated
+				// fully when they come back from the redirect. This prevents
+				// caching problems where part of the
+				// request is preformed fore the user was authenticated and the
+				// other half after it succedded. This
+				// way the user is fully authenticated from the start of the
+				// request.
+				context.setCurrentUser(null);
+
+				return new HashMap();
+			}
+		} catch (Exception ex) {
+			throw new PatternException("Unable to preform authentication", ex);
+		}
+
+		return null;
+	}
+
+}
