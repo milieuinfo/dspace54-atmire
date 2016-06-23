87021f28d36bc433131c634400644c08fe9ef2a2
Guy Wauters <guy@milieuinfo.be>
2016-06-23 11:52:04 +0200

    IMJV-5154 	Maken van een Dspace client
     Retrieve items based on given pairs of metadatafields and values WITH offset parameter.

--- dspace/modules/rest/src/main/java/org/dspace/rest/SearchResource.java
+++ dspace/modules/rest/src/main/java/org/dspace/rest/SearchResource.java
@@ -99,4 +99,71 @@ public class SearchResource extends Resource {
         return toReturn;
     }
 
+    @GET
+    @Path("/item2")
+    @ApiOperation(value = "Retrieve items based on given pairs of metadatafields and values.",
+            response = Item.class
+    )
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public List<org.dspace.rest.common.Item> searchItems2(
+
+            @ApiParam( value = "The metadatafields to use in the search, colon (;) separated, in the form of \"metadatafield:value\"", required = true)
+            @QueryParam("fields") String fields,
+
+            @ApiParam( value = "Show additional data for the item.", required = false, allowMultiple = true, allowableValues = "all,metadata,parentCollection,parentCollectionList,parentCommunityList,bitstreams")
+            @QueryParam("expand") String expand,
+
+            @ApiParam( value = "The maximum amount of items shown.", required = false)
+            @QueryParam("limit") int limit,
+            @QueryParam("offset") int offset,
+            @Context HttpHeaders headers, @Context HttpServletRequest request)
+            throws WebApplicationException, Exception {
+
+        DiscoverQuery dq = new DiscoverQuery();
+
+        dq.setDSpaceObjectFilter(org.dspace.core.Constants.ITEM);
+
+        String cleanFields = StringUtils.trimToNull(fields);
+        String cleanExpand = StringUtils.trimToNull(expand);
+
+        if (null == cleanExpand){
+            cleanExpand = "metadata";
+        }
+
+        if (null != cleanFields) {
+            String[] splitFields = StringUtils.split(cleanFields, ';');
+            String query = StringUtils.join(splitFields , " AND ");
+            dq.setQuery(query);
+        }
+
+        if (0 == limit) {
+            dq.setMaxResults(10);
+        }else{
+            dq.setMaxResults(limit);
+        }
+
+        dq.setStart(offset);
+
+
+        SearchService searchService = SearchUtils.getSearchService();
+
+        org.dspace.core.Context context = new org.dspace.core.Context();
+
+        context.turnOffAuthorisationSystem();
+
+        DiscoverResult result = searchService.search(context, dq);
+
+        List<DSpaceObject> dspaceObjects = result.getDspaceObjects();
+
+        List<org.dspace.rest.common.Item> toReturn = new ArrayList<org.dspace.rest.common.Item>();
+        for (DSpaceObject obj : dspaceObjects){
+            Item it = (Item) obj;
+            toReturn.add(new org.dspace.rest.common.Item(it,cleanExpand,context));
+        }
+
+        context.complete();
+
+        return toReturn;
+    }
+
 }
