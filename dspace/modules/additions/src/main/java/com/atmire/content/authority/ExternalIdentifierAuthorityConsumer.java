890bfa21db8d1c835b35de9b092110876f1c4723
Tom Desair <tom.desair@atmire.com>
2016-08-05 13:56:14 +0200

    Refactored external identifier consumer

--- dspace/modules/additions/src/main/java/com/atmire/content/authority/ExternalIdentifierAuthorityConsumer.java
+++ dspace/modules/additions/src/main/java/com/atmire/content/authority/ExternalIdentifierAuthorityConsumer.java
@@ -1,13 +1,18 @@
 package com.atmire.content.authority;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.dspace.content.DSpaceObject;
 import org.dspace.content.Item;
+import org.dspace.content.Metadatum;
 import org.dspace.core.ConfigurationManager;
 import org.dspace.core.Context;
 import org.dspace.event.Consumer;
 import org.dspace.event.Event;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by dylan on 30/06/16.
@@ -16,39 +21,22 @@ public class ExternalIdentifierAuthorityConsumer implements Consumer
 {
 
     private static Logger log = Logger.getLogger(ExternalIdentifierAuthorityConsumer.class);
+
+    private Set<Integer> itemsToProcess = new HashSet<>();
+
+    private String authorityField = ConfigurationManager.getProperty("authority.externalidentifier.field");
+
+
     public void initialize() throws Exception
     {
-
+        itemsToProcess.clear();
     }
-
 
     public void consume(Context context, Event event) throws Exception
     {
-
-        // get the field
-        String authorityField = ConfigurationManager.getProperty("authority.externalidentifier.field");
-
-        String[] splitField = authorityField.split("\\.");
-
-
-        String dcSchema = splitField[0];
-        String dcElement = splitField[1];
-        String dcQualifier = null;
-        if(splitField.length==3)
-        {
-            dcQualifier = splitField[2];
-        }
-
         DSpaceObject dso = event.getSubject(context);
-        Item submittedItem = (Item) dso;
-        String identifierValue = submittedItem.getMetadata(authorityField);
-        if(StringUtils.isNotEmpty(identifierValue))
-        {
-            if(StringUtils.isEmpty(submittedItem.getMetadataShallow(dcSchema,dcElement,dcQualifier,Item.ANY)[0].authority))
-            {
-                submittedItem.getMetadataShallow(dcSchema,dcElement, dcQualifier, Item.ANY)[0].authority = identifierValue;
-                submittedItem.update();
-            }
+        if(dso instanceof Item) {
+            itemsToProcess.add(dso.getID());
         }
     }
 
@@ -60,7 +48,23 @@ public class ExternalIdentifierAuthorityConsumer implements Consumer
      */
     public void end(Context context) throws Exception
     {
-        context.commit();
+        for (Integer itemId : itemsToProcess) {
+            Item item = Item.find(context, itemId);
+
+            Metadatum[] identifierValues = item.getMetadataByMetadataString(authorityField);
+            if(ArrayUtils.isNotEmpty(identifierValues)) {
+                for (Metadatum identifierValue : identifierValues) {
+                    if(StringUtils.isBlank(identifierValue.authority)) {
+                        Metadatum newValue = identifierValue.copy();
+                        newValue.authority = newValue.value;
+                        item.replaceMetadataValue(identifierValue, newValue);
+                    }
+                }
+            }
+
+        }
+
+        context.getDBConnection().commit();
     }
 
     public void finish(Context ctx) throws Exception
