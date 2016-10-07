837e1bce620cbd9d35ef119499005dcc7411b5ea
Tom Desair <tom.desair@atmire.com>
2016-10-07 14:47:41 +0200

    Improved crontab

--- dspace/src/main/resources/crontab.sh
+++ dspace/src/main/resources/crontab.sh
@@ -9,7 +9,7 @@ SHELL=/bin/bash
 
 # Set JAVA_OPTS with defaults for DSpace Cron Jobs.
 # Only provides 512MB of memory by default (which should be enough for most sites).
-JAVA_OPTS="-Xmx1024M -Xms128M -Dfile.encoding=UTF-8"
+JAVA_OPTS="-Xmx2048M -Xms128M -XX:MaxPermSize=512M -Dfile.encoding=UTF-8"
 
 #--------------
 # BIDAILY TASKS (Recommended to be run multiple times per day, if possible)
@@ -29,26 +29,23 @@ JAVA_OPTS="-Xmx1024M -Xms128M -Dfile.encoding=UTF-8"
 #----------------
 
 # Create backups of SOLR, database and assest store data
-0 1 * * * /usr/bin/flock -w 60 ~/cron-solr-backup.lock $DSPACE_DIR/bin/solr-backup.sh
-0 2 * * * /usr/bin/flock -w 60 ~/cron-db-backup.lock $DSPACE_DIR/bin/db-backup.sh
-0 3 * * * /usr/bin/flock -w 60 ~/cron-assest-store-backup.lock $DSPACE_DIR/bin/assest-store-backup.sh
+0 23 * * * /usr/bin/flock -w 60 ~/cron-solr-backup.lock $DSPACE_DIR/bin/backup_solr.sh /opt/tomcat/data/backup/solr
+#0 1 * * * /usr/bin/flock -w 60 ~/cron-db-backup.lock $DSPACE_DIR/bin/db-backup.sh -> DONE BY ACD
+0 2 * * * /usr/bin/flock -w 60 ~/cron-assest-store-backup.lock $DSPACE_DIR/bin/backup_assetstore.sh /opt/tomcat/data/backup/assetstore
 
-# Run any Curation Tasks queued from the Admin UI at 04:00 every day
+# Create an incremental export starting at 3 am every day
+0 3 * * 5 /usr/bin/flock -w 60 ~/cron-export.lock $DSPACE_DIR/bin/dspace export -g -m -c -t COLLECTION -i acd/2 -d /opt/tomcat/data/export/IMJV
+0 5 * * 5 /usr/bin/flock -w 60 ~/cron-export.lock $DSPACE_DIR/bin/dspace export -g -m -c -t COLLECTION -i acd/4 -d /opt/tomcat/data/export/DBA
+0 7 * * 5 /usr/bin/flock -w 60 ~/cron-export.lock $DSPACE_DIR/bin/dspace export -g -m -c -t COLLECTION -i acd/5 -d /opt/tomcat/data/export/OMV
+
+# Run any Curation Tasks queued from the Admin UI at 22:00 every day
 # (Ensures that any curation task that an administrator "queued" from the Admin UI is executed
 # asynchronously behind the scenes)
-0 4 * * * /usr/bin/flock -w 60 ~/cron-curation.lock $DSPACE_DIR/bin/dspace curate -q admin_ui
+0 22 * * * /usr/bin/flock -w 60 ~/cron-curation.lock $DSPACE_DIR/bin/dspace curate -q admin_ui
 
 # Send out subscription e-mails at 22:00 every day
 # (This sends an email to any users who have "subscribed" to a Collection, notifying them of newly added content.)
 0 22 * * * /usr/bin/flock -w 60 ~/cron-subscriptions.lock $DSPACE_DIR/bin/dspace sub-daily
-
-# Clean and Update the Discovery indexes every day
-# (This ensures that any deleted documents are cleaned from the Discovery search/browse index)
-0 3 * * * /usr/bin/flock -w 60 ~/cron-discovery.lock $DSPACE_DIR/bin/dspace index-discovery
-
-# Run the index-authority script once a day at 5 am to ensure the Solr Authority cache is up to date
-0 5 * * * /usr/bin/flock -w 60 ~/cron-authority.lock $DSPACE_DIR/bin/dspace index-authority
-
 
 #----------------
 # WEEKLY TASKS
@@ -60,18 +57,15 @@ JAVA_OPTS="-Xmx1024M -Xms128M -Dfile.encoding=UTF-8"
 0 20 * * 6 /usr/bin/flock -w 60 ~/cron-authority.lock $DSPACE_DIR/bin/dspace index-authority -o
 0 22 * * 6 /usr/bin/flock -w 60 ~/cron-statistics.lock $DSPACE_DIR/bin/dspace stats-util -o
 
-# Run the checksum checker at 03:00 every Sunday
-0 3 * * 0 /usr/bin/flock -w 60 ~/cron-checksum.lock $DSPACE_DIR/bin/dspace checker -d 4h -p
+# Run the checksum checker at 04:00 every Sunday
+0 4 * * 0 /usr/bin/flock -w 60 ~/cron-checksum.lock $DSPACE_DIR/bin/dspace checker -d 4h -p
 
 # Mail the results of the checksum checker (see above) to the configured "mail.admin" at 08:00 every Sunday.
 # (This ensures the system administrator is notified whether any checksums were found to be different.)
-0 8 * * 0 /usr/bin/flock -w 60 ~/cron-checksum.lock $DSPACE_DIR/bin/dspace checker-emailer
+0 9 * * 0 /usr/bin/flock -w 60 ~/cron-checksum.lock $DSPACE_DIR/bin/dspace checker-emailer
 
-# Run the virus scanner at 03:00 every Saturday
-0 3 * * 6 /usr/bin/flock -w 60 ~/cron-virusscan.lock $DSPACE_DIR/bin/dspace check-bitstreams -d 4h -p vscan
-
-# Create an export at 23:00 every Friday
-0 23 * * 5 /usr/bin/flock -w 60 ~/cron-export.lock $DSPACE_DIR/bin/dspace export -g -m -t COLLECTION -i <collection handle> -d /path/to/export/location
+# Run the virus scanner at 04:00 every Saturday
+0 4 * * 6 /usr/bin/flock -w 60 ~/cron-virusscan.lock $DSPACE_DIR/bin/dspace check-bitstreams -d 4h -p vscan
 
 #----------------
 # MONTHLY TASKS
@@ -79,4 +73,5 @@ JAVA_OPTS="-Xmx1024M -Xms128M -Dfile.encoding=UTF-8"
 # Permanently delete any bitstreams flagged as "deleted" in DSpace, on the first of every month at 01:00
 # (This ensures that any files which were deleted from DSpace are actually removed from your local filesystem.
 #  By default they are just marked as deleted, but are not removed from the filesystem.)
-0 1 1 * * /usr/bin/flock -w 60 ~/cron-cleanup.lock $DSPACE_DIR/bin/dspace cleanup
+# ATTENTION: ACD wants to keep all bitsreams, hence this job should not run automatically
+#0 1 1 * * /usr/bin/flock -w 60 ~/cron-cleanup.lock $DSPACE_DIR/bin/dspace cleanup
