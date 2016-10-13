2145f4db179df45ede0aca7c6becc4748f9b5257
Lodewijk De Schuyter <lds@milieuinfo.be>
2016-10-13 17:55:36 +0200

    Add config clamav

--- dspace/src/main/resources/postinstall.sh
+++ dspace/src/main/resources/postinstall.sh
@@ -148,4 +148,145 @@ rm -rf /tmp/GeoLiteCity.dat.gz
 
 crontab -u tomcat ${tomcat_apps_dir}/crontab.sh
 
+echo "Config ClamAV"
 
+cat << EOF > /etc/clamav/clamd.conf
+TCPSocket 3310
+TCPAddr 127.0.0.1
+# TemporaryDirectory is not set to its default /tmp here to make overriding
+# the default with environment variables TMPDIR/TMP/TEMP possible
+User clamav
+AllowSupplementaryGroups false
+ScanMail true
+ScanArchive true
+ArchiveBlockEncrypted false
+MaxDirectoryRecursion 15
+FollowDirectorySymlinks false
+FollowFileSymlinks false
+ReadTimeout 180
+MaxThreads 12
+MaxConnectionQueueLength 15
+LogSyslog true
+LogRotate true
+LogFacility LOG_LOCAL6
+LogClean false
+LogVerbose false
+DatabaseDirectory /var/lib/clamav
+OfficialDatabaseOnly false
+SelfCheck 3600
+Foreground false
+Debug false
+ScanPE true
+MaxEmbeddedPE 10M
+ScanOLE2 true
+ScanPDF true
+ScanHTML true
+MaxHTMLNormalize 10M
+MaxHTMLNoTags 2M
+MaxScriptNormalize 5M
+MaxZipTypeRcg 1M
+ScanSWF true
+DetectBrokenExecutables false
+ExitOnOOM false
+LeaveTemporaryFiles false
+AlgorithmicDetection true
+ScanELF true
+IdleTimeout 30
+CrossFilesystems true
+PhishingSignatures true
+PhishingScanURLs true
+PhishingAlwaysBlockSSLMismatch false
+PhishingAlwaysBlockCloak false
+PartitionIntersection false
+DetectPUA false
+ScanPartialMessages false
+HeuristicScanPrecedence false
+StructuredDataDetection false
+CommandReadTimeout 5
+SendBufTimeout 200
+MaxQueue 100
+ExtendedDetectionInfo true
+OLE2BlockMacros false
+ScanOnAccess false
+AllowAllMatchScan true
+ForceToDisk false
+DisableCertCheck false
+DisableCache false
+MaxScanSize 100M
+MaxFileSize 25M
+MaxRecursion 16
+MaxFiles 10000
+MaxPartitions 50
+MaxIconsPE 100
+PCREMatchLimit 10000
+PCRERecMatchLimit 5000
+PCREMaxFileSize 25M
+StatsEnabled false
+StatsPEDisabled true
+StatsHostID auto
+StatsTimeout 10
+StreamMaxLength 100M
+LogFile /var/log/clamav/clamav.log
+LogTime true
+LogFileUnlock false
+LogFileMaxSize 0
+Bytecode true
+BytecodeSecurity TrustSigned
+BytecodeTimeout 60000
+EOF
+
+
+
+
+cat << EOF > /etc/clamav/freshclam.conf
+DatabaseOwner clamav
+UpdateLogFile /var/log/clamav/freshclam.log
+LogVerbose false
+LogSyslog false
+LogFacility LOG_LOCAL6
+LogFileMaxSize 0
+LogRotate true
+LogTime true
+Foreground false
+Debug false
+MaxAttempts 5
+DatabaseDirectory /var/lib/clamav
+DNSDatabaseInfo current.cvd.clamav.net
+AllowSupplementaryGroups false
+ConnectTimeout 30
+ReceiveTimeout 30
+TestDatabases yes
+ScriptedUpdates yes
+CompressLocalDatabase no
+SafeBrowsing false
+Bytecode true
+NotifyClamd /etc/clamav/clamd.conf
+# Check for new database 24 times a day
+Checks 24
+DatabaseMirror db.local.clamav.net
+DatabaseMirror database.clamav.net
+HTTPProxyServer ${http_proxy_host}
+HTTPProxyPort ${http_proxy_port}
+EOF
+
+chown clamav:clamav /etc/clamav/freshclam.conf
+
+cat << EOF > /etc/systemd/system/clamav-daemon.socket.d/extend.conf
+[Socket]
+ListenStream=
+SocketUser=clamav
+ListenStream=127.0.0.1:3310
+EOF
+systemctl stop clamav-freshclam.service
+
+freshclam
+
+echo reload daemons
+systemctl --system daemon-reload
+systemctl start clamav-daemon.socket
+
+echo start clamav
+systemctl start clamav-daemon.service
+
+echo start fleshclam
+systemctl start clamav-freshclam.service
