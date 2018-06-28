8dc63b3a25d49b9c0d5cad79ad76920545b66173
Tom Desair <tom.desair@atmire.com>
2018-06-28 12:08:25 +0200

    Fix MilieuinfoComplianceStep

--- dspace/modules/additions/src/main/java/com/atmire/submission/MilieuinfoComplianceStep.java
+++ dspace/modules/additions/src/main/java/com/atmire/submission/MilieuinfoComplianceStep.java
@@ -6,11 +6,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.atmire.sword.result.ComplianceResult;
+import com.atmire.sword.service.ComplianceCheckService;
+import org.apache.commons.lang.StringUtils;
 import org.dspace.app.util.SubmissionInfo;
 import org.dspace.app.util.Util;
 import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.Item;
+import org.dspace.core.ConfigurationManager;
 import org.dspace.core.Context;
 import org.dspace.submit.AbstractProcessingStep;
+import org.dspace.utils.DSpace;
 
 /**
  * Created by Philip Vissenaekens (philip at atmire dot com)
@@ -20,6 +26,12 @@ import org.dspace.submit.AbstractProcessingStep;
 public class MilieuinfoComplianceStep extends AbstractProcessingStep {
 
     public static final int STATUS_BLOCKED = 2;
+
+    private static final String blockSubmissionConfig = "milieuinfo.submission.block.on.rule.violation.";
+    private static final String defaultConfig = "default";
+
+    private static ComplianceCheckService complianceCheckService = new DSpace().getServiceManager()
+            .getServiceByName("milieuinfoComplianceCheckService", ComplianceCheckService.class);
 
     @Override
     public int doProcessing(
@@ -31,10 +43,32 @@ public class MilieuinfoComplianceStep extends AbstractProcessingStep {
         String buttonPressed = Util.getSubmitButton(request, NEXT_BUTTON);
 
         if (NEXT_BUTTON.equals(buttonPressed)) {
-            return STATUS_COMPLETE;
-        } else {
-            return STATUS_BLOCKED;
+
+            Item item = subInfo.getSubmissionItem().getItem();
+
+            String blockSubmissionOnViolation = ConfigurationManager.getProperty(
+                    "item-compliance",
+                    blockSubmissionConfig + subInfo.getCollectionHandle()
+            );
+
+            if (StringUtils.isBlank(blockSubmissionOnViolation)) {
+                blockSubmissionOnViolation = ConfigurationManager.getProperty(
+                        "item-compliance",
+                        blockSubmissionConfig + defaultConfig
+                );
+            }
+
+            if (StringUtils.isNotBlank(blockSubmissionOnViolation) && Boolean.parseBoolean(
+                    blockSubmissionOnViolation)) {
+                ComplianceResult result = complianceCheckService.checkCompliance(context, item);
+
+                if (!result.isCompliant()) {
+                    return STATUS_BLOCKED;
+                }
+            }
         }
+
+        return STATUS_COMPLETE;
     }
 
     @Override
